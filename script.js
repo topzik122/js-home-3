6348e8e68539fb709c5aa3bd10ce21ce1f1ada88
Egor <cefranovegor@gmail.com>
2024-09-26 18:25:53 +0300

    Добавление изминений

---
+++ script.js
@@ -0,0 +1,61 @@
+const userInput1= +prompt("введите число от 0 до 9");
+
+
+//function ageNumber(name, number) {
+    //if (number >= 0 && number <= 2) {
+    //    return "Вы ребёнок";
+  //  } else if (number >= 12 && number <= 18) {
+  //      return "Вы подросток";
+  //  } else if (number >= 18 && number <= 60) {
+   //     return "Вы взрослый";
+  //  } else if (number > 60) {
+ //       return "Вы пенсионер";
+ //   } else {
+  //      return "Некорректный ввод";
+  //  }
+//}
+
+// Теперь вызываем функцию и выводим результат
+//alert(result);  // Выводим результат на экран
+
+//function numbersimvol(number){
+   // switch (userInput) {
+       // case 1:
+        //    alert("!");
+        //    break;
+      //  case 2:
+         //   alert ("@")
+       //     break;
+      //  case 3:
+       //     alert ("#")
+        //    break
+      //  case 4:
+        //    alert ("$")
+        //    break
+       // case 5:
+        //    alert ("%")
+       //     break
+      //  case 6:
+        //    alert ("^")
+      //      break
+       // case 7:
+       //     alert ("&")
+       //     break
+       // case 8:
+      //       alert ("*")
+       //      break
+      //  case 9:
+        //     alert ("(")
+         //    break
+         //    default:
+            //    alert("Неправильный ввод. Введите число от 0 до 9.");      
+   // }
+//}
+
+function money(number){
+    
+}
+
+
+
+const result = umbersimvo(number);
