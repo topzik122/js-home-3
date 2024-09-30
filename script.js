a9eb4beba86766a529d8a3ca8824a12fca360e06
Egor <cefranovegor@gmail.com>
2024-09-30 20:05:52 +0300

    добавленны изминения

--- script.js
+++ script.js
@@ -1,61 +1,58 @@
-const userInput1= +prompt("введите число от 0 до 9");
+let number1= +prompt("введите возраст");
 
 
-//function ageNumber(name, number) {
-    //if (number >= 0 && number <= 2) {
-    //    return "Вы ребёнок";
-  //  } else if (number >= 12 && number <= 18) {
-  //      return "Вы подросток";
-  //  } else if (number >= 18 && number <= 60) {
-   //     return "Вы взрослый";
-  //  } else if (number > 60) {
- //       return "Вы пенсионер";
- //   } else {
-  //      return "Некорректный ввод";
-  //  }
-//}
 
-// Теперь вызываем функцию и выводим результат
-//alert(result);  // Выводим результат на экран
-
-//function numbersimvol(number){
-   // switch (userInput) {
-       // case 1:
-        //    alert("!");
-        //    break;
-      //  case 2:
-         //   alert ("@")
-       //     break;
-      //  case 3:
-       //     alert ("#")
-        //    break
-      //  case 4:
-        //    alert ("$")
-        //    break
-       // case 5:
-        //    alert ("%")
-       //     break
-      //  case 6:
-        //    alert ("^")
-      //      break
-       // case 7:
-       //     alert ("&")
-       //     break
-       // case 8:
-      //       alert ("*")
-       //      break
-      //  case 9:
-        //     alert ("(")
-         //    break
-         //    default:
-            //    alert("Неправильный ввод. Введите число от 0 до 9.");      
-   // }
-//}
-
-function money(number){
-    
+function ageNumber(name, number) {
+    if (number >= 0 && number <= 2) {
+       return "Вы ребёнок";
+   } else if (number >= 12 && number <= 18) {
+       return "Вы подросток";
+   } else if (number >= 18 && number <= 60) {
+       return "Вы взрослый";
+   } else if (number > 60) {
+       return "Вы пенсионер";
+   } else {
+       return "Некорректный ввод";
+   }
 }
 
 
+alert(result);  // Выводим результат на экран
+numbersimvol(number1)
+function numbersimvol(number) {
+    switch (number) {
+        case 1:
+          return("!");
+           break;
+      case 2:
+            return ("@")
+            break;
+        case 3:
+            return ("#")
+            break
+        case 4:
+           return ("$")
+           break
+        case 5:
+            return ("%")
+            break
+       case 6:
+           return ("^")
+            break
+        case 7:
+            return ("&")
+            break
+        case 8:
+             return ("*")
+            break
+       case 9:
+             return ("(")
+             break
+            default:
+               return("Неправильный ввод. Введите число от 0 до 9.");      
+    }
+}
+alert(numbersimvol(number1))
 
-const result = umbersimvo(number);
+
+
